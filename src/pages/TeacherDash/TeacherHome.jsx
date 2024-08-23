260bed419a928c6c7ac7ca02e0338e3391d51402
Soheb_Patwary <128284622+shossen1999@users.noreply.github.com>
2024-08-23 12:19:30 +0600

    Delete src/pages/TeacherDash/TeacherHome.jsx

--- src/pages/TeacherDash/TeacherHome.jsx
+++
@@ -1,9 +0,0 @@
-const TeacherHome = () => {
-    return (
-        <div>
-            <h1 className="text-2xl md:text-5xl font-bold text-center">Welcome Back Teacher</h1>
-        </div>
-    );
-};
-
-export default TeacherHome;
