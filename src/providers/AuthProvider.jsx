082914d5e1bc483235eb4bea4ff6364e4b9152a7
shossen1999 <shossen1999@gmail.com>
2024-08-25 22:06:29 +0600

    last modification on eduFusion

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -2,7 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import auth from './../firebase/firebase.config';
 import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import useAxiosPublic from "../Hooks/useAxiosPublic";
-// import useAxiosPublic from "../../hooks/useAxiosPublic";
+
 
 export const AuthContext = createContext()
 const AuthProvider = ({ children }) => {
