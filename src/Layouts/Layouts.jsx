9ac65aff2ae720d41e6704ca2ff267d38e1b9a60
shossen1999 <shossen1999@gmail.com>
2024-08-30 14:29:45 +0600

    last comment

--- src/Layouts/Layouts.jsx
+++ src/Layouts/Layouts.jsx
@@ -1,8 +1,9 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
 import Footer from "../Components/Footer/Footer";
-import { ToastContainer } from "react-toastify";
+// import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
 
 const Layouts = () => {
   return (
